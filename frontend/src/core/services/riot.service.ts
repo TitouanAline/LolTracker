67a263465a92f61c0f60b904a1f1c2c506496ad0
titouan-aline-theodo <titouan.aline@theodo.com>
2026-04-21 11:15:22 +0200

    Refacto : moving component logic into service

--- frontend/src/core/services/riot.service.ts
+++ frontend/src/core/services/riot.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, forkJoin, map, Observable, of, switchMap } from 'rxjs';
 
 import { SummonerDto } from '../models/summoner.dto';
 import { SummonerGameDetailsDto } from '../models/summoner-game-details.dto';
@@ -17,8 +17,46 @@ export class RiotService {
     return this.http.get<SummonerDto>(`${this.apiUrl}/${name}/${tag}`);
   }
 
-  // récupérer dernier champion
   getGame(puuid: string, index: number): Observable<SummonerGameDetailsDto> {
     return this.http.get<SummonerGameDetailsDto>(`${this.apiUrl}/${puuid}/game/${index}`);
+  }
+
+  getPlayerWithGame(name: string, tag: string) {
+    return this.getSummoner(name, tag).pipe(switchMap((account) => this.getGame(account.puuid, 0)));
+  }
+
+  getFriendsGames(friends: { name: string; tag: string }[]) {
+    return forkJoin(
+      friends.map((f) =>
+        this.getPlayerWithGame(f.name, f.tag).pipe(
+          map((match) => ({
+            name: f.name,
+            tag: f.tag,
+            champion: match.champion,
+            championImage: match.championIcon,
+            splashImage: match.championSplashArt,
+            kills: match.kills,
+            deaths: match.deaths,
+            assists: match.assists,
+            win: match.win,
+            error: false,
+          })),
+          catchError(() =>
+            of({
+              name: f.name,
+              tag: f.tag,
+              champion: null,
+              championImage: null,
+              splashImage: null,
+              kills: null,
+              deaths: null,
+              assists: null,
+              win: null,
+              error: true,
+            }),
+          ),
+        ),
+      ),
+    );
   }
 }
--- frontend/src/features/summoner/summoner.ts
+++ frontend/src/features/summoner/summoner.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RiotService } from '../../core/services/riot.service';
-import { BehaviorSubject, switchMap, map, catchError, of, tap, forkJoin } from 'rxjs';
-import { SummonerDto } from '../../core/models/summoner.dto';
-import { SummonerGameDetailsDto } from '../../core/models/summoner-game-details.dto';
+import { OnInit } from '@angular/core';
+import { BehaviorSubject, switchMap, map, catchError, of, tap, forkJoin, Observable } from 'rxjs';
 import { FriendGameDetailDto } from '../../core/models/friend-game-details.dto';
 
 @Component({
@@ -14,8 +13,21 @@ import { FriendGameDetailDto } from '../../core/models/friend-game-details.dto';
   templateUrl: './summoner.html',
   styleUrls: ['./summoner.css'],
 })
-export class SummonerComponent {
+export class SummonerComponent implements OnInit {
+  friends = [
+    { name: 'Fyralll', tag: '5403' },
+    { name: 'G2 SkewMond', tag: '3327' },
+    { name: 'G2 Caps', tag: '1323' },
+    { name: 'Nathanzor', tag: 'EUW' },
+  ];
+
+  friends$!: Observable<FriendGameDetailDto[]>;
+
   constructor(private riotService: RiotService) {}
+
+  ngOnInit(): void {
+    this.friends$ = this.riotService.getFriendsGames(this.friends);
+  }
 
   name = '';
   tag = '';
@@ -23,10 +35,8 @@ export class SummonerComponent {
   loading = false;
   error = '';
 
-  // 🔥 trigger
   private searchTrigger = new BehaviorSubject<{ name: string; tag: string } | null>(null);
 
-  // 🔥 RESULT PRINCIPAL
   result$ = this.searchTrigger.pipe(
     tap(() => {
       this.loading = true;
@@ -36,13 +46,8 @@ export class SummonerComponent {
     switchMap((params) => {
       if (!params) return of(null);
 
-      return this.riotService.getSummoner(params.name, params.tag).pipe(
-        switchMap((account) => this.riotService.getGame(account.puuid, 0)),
-
-        map((data) => data),
-
-        catchError((err) => {
-          console.error(err);
+      return this.riotService.getPlayerWithGame(params.name, params.tag).pipe(
+        catchError(() => {
           this.error = 'Erreur API';
           return of(null);
         }),
@@ -52,53 +57,6 @@ export class SummonerComponent {
     tap(() => (this.loading = false)),
   );
 
-  // 🔥 FRIENDS LIST
-  friends = [
-    { name: 'Fyralll', tag: '5403' },
-    { name: 'G2 SkewMond', tag: '3327' },
-    { name: 'G2 Caps', tag: '1323' },
-    { name: 'Nathanzor', tag: 'EUW' },
-  ];
-
-  friends$ = of(this.friends).pipe(
-    switchMap((friends) =>
-      forkJoin(
-        friends.map((f) =>
-          this.riotService.getSummoner(f.name, f.tag).pipe(
-            switchMap((account) => this.riotService.getGame(account.puuid, 0)),
-            map(
-              (match): FriendGameDetailDto => ({
-                name: f.name,
-                tag: f.tag,
-                champion: match.champion,
-                championImage: match.championIcon,
-                splashImage: match.championSplashArt,
-                kills: match.kills,
-                deaths: match.deaths,
-                assists: match.assists,
-                win: match.win,
-                error: false,
-              }),
-            ),
-            catchError(() =>
-              of({
-                name: f.name,
-                tag: f.tag,
-                champion: null,
-                championImage: null,
-                splashImage: null,
-                kills: null,
-                deaths: null,
-                assists: null,
-                win: null,
-                error: true,
-              }),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
   search() {
     console.log('CLICK OK');
 

c495eabffe7e1bb67763453eaaa1d74e6221ae43
titouan-aline-theodo <titouan.aline@theodo.com>
2026-04-21 14:10:17 +0200

    Refacto : observables -> signals

--- frontend/src/core/services/riot.service.ts
+++ frontend/src/core/services/riot.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, forkJoin, map, Observable, of, switchMap } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 
 import { SummonerDto } from '../models/summoner.dto';
 import { SummonerGameDetailsDto } from '../models/summoner-game-details.dto';
--- frontend/src/features/summoner/summoner.ts
+++ frontend/src/features/summoner/summoner.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RiotService } from '../../core/services/riot.service';
-import { OnInit } from '@angular/core';
-import { BehaviorSubject, switchMap, map, catchError, of, tap, forkJoin, Observable } from 'rxjs';
+import { finalize } from 'rxjs';
+
 import { FriendGameDetailDto } from '../../core/models/friend-game-details.dto';
+import { SummonerGameDetailsDto } from '../../core/models/summoner-game-details.dto';
 
 @Component({
   selector: 'app-summoner',
@@ -14,53 +15,35 @@ import { FriendGameDetailDto } from '../../core/models/friend-game-details.dto';
   styleUrls: ['./summoner.css'],
 })
 export class SummonerComponent implements OnInit {
-  friends$!: Observable<FriendGameDetailDto[]>;
-
   constructor(private riotService: RiotService) {}
 
-  ngOnInit(): void {
-    this.friends$ = this.riotService.getFriendsGames();
+  name = signal('');
+  tag = signal('');
+
+  loading = signal(false);
+  error = signal('');
+  result = signal<SummonerGameDetailsDto | null>(null);
+  friends = signal<FriendGameDetailDto[]>([]);
+
+  ngOnInit() {
+    this.riotService.getFriendsGames().subscribe({
+      next: (data) => this.friends.set(data),
+      error: () => this.error.set('Erreur chargement friends'),
+    });
   }
 
-  name = '';
-  tag = '';
-
-  loading = false;
-  error = '';
-
-  private searchTrigger = new BehaviorSubject<{ name: string; tag: string } | null>(null);
-
-  result$ = this.searchTrigger.pipe(
-    tap(() => {
-      this.loading = true;
-      this.error = '';
-    }),
-
-    switchMap((params) => {
-      if (!params) return of(null);
-
-      return this.riotService.getPlayerWithGame(params.name, params.tag).pipe(
-        catchError(() => {
-          this.error = 'Erreur API';
-          return of(null);
-        }),
-      );
-    }),
-
-    tap(() => (this.loading = false)),
-  );
-
   search() {
-    console.log('CLICK OK');
+    if (!this.name() || !this.tag()) return;
 
-    if (!this.name || !this.tag) {
-      console.log('❌ Champs vides');
-      return;
-    }
+    this.loading.set(true);
+    this.error.set('');
 
-    this.searchTrigger.next({
-      name: this.name,
-      tag: this.tag,
-    });
+    this.riotService
+      .getPlayerWithGame(this.name(), this.tag())
+      .pipe(finalize(() => this.loading.set(false)))
+      .subscribe({
+        next: (data) => this.result.set(data),
+        error: () => this.error.set('Erreur API'),
+      });
   }
 }
